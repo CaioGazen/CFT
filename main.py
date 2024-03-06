fb1a00f96ed7d70a57ad577e905515e9fc1883da
Caio Gazen <61031028+CaioGazen@users.noreply.github.com>
2024-03-06 16:56:21 -0300

    refactor

--- main.py
+++ main.py
@@ -1,118 +1,125 @@
-from svg.path import Path, Line, Arc, CubicBezier, QuadraticBezier, parse_path
+from svg.path import parse_path
 from xml.dom import minidom
 import numpy as np
 import pygame
 
 
-
 # read the SVG file
-doc = minidom.parse('cube.svg')
-path_strings = [path.getAttribute('d') for path in doc.getElementsByTagName('path')]
+doc = minidom.parse("cube.svg")
+path_strings = [path.getAttribute("d") for path in doc.getElementsByTagName("path")]
 doc.unlink()
 
-svgpath =  path_strings[0]
-
+svgpath = path_strings[0]
 
 path = parse_path(svgpath)
+
 
 def path_func(t):
     p = path.point(t)
     return p
 
+
 def calcular_c(n, t):
-    return ((path_func(t) * (np.exp(-1* n * 2 *np.pi * 1j *t ))))
+    return path_func(t) * (np.exp(-1 * n * 2 * np.pi * 1j * t))
 
 
 def midpoint(func, fn, a, b, n):
-    if n % 2 != 0: raise Exception("n não é par")
+    if n % 2 != 0:
+        raise Exception("n não é par")
 
     soma = 0
-    deltaX = (b - a)/n
-    pos_atual = a + (deltaX/2)
+    deltaX = (b - a) / n
+    pos_atual = a + (deltaX / 2)
 
     for i in range(n):
         soma += deltaX * func(fn, pos_atual)
         pos_atual += deltaX
 
-    return (soma)
-    
+    return soma
 
-def vetor_c(n_vetores):
+
+def get_coefficients(n_vetores):
     cn = []
     for n in range((n_vetores // 2) * -1, (n_vetores // 2) + 1, 1):
         cn.append((n, midpoint(calcular_c, n, 0, 1, 1000)))
 
     return cn
-    
+
+
 def soma_vetores(t, cn, scale, x_offset, y_offset):
-    soma = 0
-    cs = []
-    vectors = [(0,0)]
+    vectors = [(0, 0)]
     for n, c in cn:
         vector = c * np.exp(n * 2 * np.pi * 1j * t)
-        soma += vector
-        vectors.append(((vector.real) + vectors[len(vectors) - 1][0], (vector.imag)  + vectors[len(vectors) - 1][1]))
+        vectors.append(
+            (
+                (vector.real) + vectors[len(vectors) - 1][0],
+                (vector.imag) + vectors[len(vectors) - 1][1],
+            )
+        )
 
-    vectors = (np.array(vectors)*scale) + (x_offset, y_offset)
-    return soma, vectors
+    vectors = (np.array(vectors) * scale) + (x_offset, y_offset)
+    return vectors
+
 
 def main():
     window_size = 600
-    x_offset = window_size/2
-    y_offset = window_size/2
+    x_offset = window_size / 2
+    y_offset = window_size / 2
     scale = 15
 
     n_vectors = 100
 
     # original svg
     n_lines = 1000
-    pts = [ ((p.real * scale) + x_offset ,(p.imag * scale) + y_offset ) for p in (path.point(i/n) for i in range(0, n+1))]
+    svg_points = [
+        ((p.real * scale) + x_offset, (p.imag * scale) + y_offset)
+        for p in (path.point(i / n_lines) for i in range(0, n_lines + 1))
+    ]
 
+    coefficients = get_coefficients(n_vectors)
 
-    cn =   (n_vetores)
     mag_vectors = []
-    for n, c in cn:
-        x = c * np.exp(n * 2 * np.pi * 1j * 1)
+    for n_lines, c in coefficients:
+        x = c * np.exp(n_lines * 2 * np.pi * 1j * 1)
         mag_vectors.append(np.linalg.norm(x) * scale)
-        
 
+    pygame.init()  # init pygame
+    surface = pygame.display.set_mode(
+        (window_size, window_size)
+    )  # get surface to draw on
+    surface.fill(pygame.Color("white"))  # set background to white
 
-    pygame.init()                                  # init pygame
-    surface = pygame.display.set_mode((window_size,window_size))   # get surface to draw on
-    surface.fill(pygame.Color('white'))            # set background to white
-    
-    points = []
+    drawn_points = []
     time = 0
 
     while True:  # loop to wait till window close
-        if time > 1: 
+        if time > 1:
             print("loop")
             time = 0
-            surface.fill(pygame.Color('white'))
+            surface.fill(pygame.Color("white"))
 
-        surface.fill(pygame.Color('white'))
-        
-        pygame.draw.aalines(surface,pygame.Color('black'), False, pts)
+        surface.fill(pygame.Color("white"))
 
+        pygame.draw.aalines(surface, pygame.Color("black"), False, svg_points)
 
-        now, vectors = soma_vetores(time, cn, scale, x_offset, y_offset)
+        vectors = soma_vetores(time, coefficients, scale, x_offset, y_offset)
 
-        pygame.draw.lines(surface,pygame.Color('blue'), False, vectors, 2)
-        points.append(vectors[len(vectors) - 1])
+        pygame.draw.lines(surface, pygame.Color("blue"), False, vectors, 2)
+        drawn_points.append(vectors[len(vectors) - 1])
 
+        # print(vectors)
 
-        #print(vectors)
-
-        #pygame.draw.circle(surface, (255, 255, 0), (0,0), 100)
+        # pygame.draw.circle(surface, (255, 255, 0), (0,0), 100)
 
         for i in range(len(vectors)):
-            pygame.draw.circle(surface, (200, 0, 0), vectors[i-1], mag_vectors[i-1] ,1)
+            pygame.draw.circle(
+                surface, (200, 0, 0), vectors[i - 1], mag_vectors[i - 1], 1
+            )
 
-        for point in points:
+        for point in drawn_points:
             pygame.draw.circle(surface, (255, 0, 0), point, 1)
-        
-        pygame.display.update() # copy surface to display   
 
+        pygame.display.update()  # copy surface to display
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
